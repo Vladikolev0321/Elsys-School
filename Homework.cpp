efb01d59b5696dacfc2917d1914f9e046067a4b4
Vladikolev0321 <48590126+Vladikolev0321@users.noreply.github.com>
2020-12-05 23:24:31 +0200

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <list>
+#include <vector>
 using namespace std;
 
-class NetworkPackage {
+class Package {
 	char* content;
 	int content_length;
 	string ip_sent;
@@ -11,7 +13,7 @@ class NetworkPackage {
 	int number_of_packet;
 public:
 	//static int created_packet_counter = 0;
-	NetworkPackage(char *content, int content_length, string ip_sent,string ip_to)
+	Package(char *content, int content_length, string ip_sent,string ip_to)
 	{
 		if (strlen(content) == 0) {
 			throw "Content is empty";
@@ -28,6 +30,10 @@ public:
 		this->ip_sent = ip_sent;
 		this->ip_to = ip_to;
 		this->number_of_packet = created_packet_counter;
+	}
+	string get_ip_to()const
+	{
+		return this->ip_to;
 	}
 	int validate()
 	{	
@@ -48,10 +54,101 @@ public:
 		}
 		return 0;
 	}
-	~NetworkPackage()
+	~Package()
 	{
 		delete[] this->content;
 	}
+
+};
+class Router {
+	string name;
+	string ip_address;
+	vector<Router> routers_connected_to;
+	class InformationForKnownRoutes {
+		string ip;
+		int index_in_routers;
+		int count_sent_packets;
+	public:
+
+		InformationForKnownRoutes(string ip, int index)
+		{
+			this->ip = ip;
+			this->index_in_routers = index;
+		}
+
+		string get_ip()
+		{
+			return this->ip;
+		}
+
+		void increase_count_sent_packets()
+		{
+			this->count_sent_packets++;
+		}
+	};
+	list<InformationForKnownRoutes> routing_table;
+public:
+
+	Router(string name, string ip_address)
+	{
+		const static int max_count_elem_in_routing_table;
+		const static int max_hops;
+		this->name = name;
+		this->ip_address = ip_address;
+	}
+
+	void add_router(const Router& router)
+	{
+		routers_connected_to.push_back(router);
+	}
+
+	int query_route(const string address, const int hop_count)
+	{
+		if (address == this->ip_address)
+		{
+			return 1;
+		}
+		for (list<InformationForKnownRoutes>::iterator it = this->routing_table.begin(); it != this->routing_table.end(); it++)
+		{
+			InformationForKnownRoutes currInfo= *it;
+			if (currInfo.get_ip() == address)
+			{
+				return 1;
+			}
+		}
+		if (hop_count > 1)
+		{/////is != or <
+		///hops is not used to ask!!!!!!!!!!!!!
+			int i = 0;
+			for (vector<Router>::iterator it = this->routers_connected_to.begin(); it != this->routers_connected_to.end(); it++,i++)
+			{
+				Router currRouter = *it;
+				if (currRouter.query_route(address, hop_count - 1) == 1)
+				{
+					routing_table.push_back(InformationForKnownRoutes(address, i));
+					return 1;
+				}
+			}
+			return 0;
+		}
+
+	}
+	void send_package(const Package& package)
+	{
+		if (this->ip_address != package.get_ip_to())
+		{
+			for (list<InformationForKnownRoutes>::iterator it = this->routing_table.begin(); it != this->routing_table.end(); it++)
+			{
+				InformationForKnownRoutes currInfo = *it;
+				if (currInfo.get_ip() == package.get_ip_to())
+				{
+					currInfo.increase_count_sent_packets();
+					break;
+				}
+			}
+		}
+	}
+	
 
 };
 int main()
