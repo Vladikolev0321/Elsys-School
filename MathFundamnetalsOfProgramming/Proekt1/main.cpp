3fc0b0a3b885f60db29a783374a24c943d64baf4
Vladikolev0321 <48590126+Vladikolev0321@users.noreply.github.com>
2020-12-13 00:28:24 +0200

    Update main.cpp

--- MathFundamnetalsOfProgramming/Proekt1/main.cpp
+++ MathFundamnetalsOfProgramming/Proekt1/main.cpp
@@ -6,11 +6,53 @@
 using namespace std;
 
 const int M = 4;
-void Gaus(double A[][M],int n)
-{
-	double X[n] = {0};
 
-	for(int i = 0; i < n;i++)
+void print_matrix(double A[][M],int n, int m)
+{
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout<<A[i][j]<<" ";
+
+		}
+		cout << endl;
+
+	}
+}
+void Gaus(double A[][5], double X[], int n)
+{
+
+	for(int i = 0;i < 4;i++)
+	{
+		for(int k = i+1;k < 4;k++)
+		{
+			double num = -(A[k][i]/A[i][i]);
+			for(int j = i;j <= 4;j++)
+			{
+				A[k][j] += A[i][j]*num;
+			}
+		}
+	}
+
+	for(int i = 4-1;i>=0;--i)
+	{
+		X[i] = A[i][4]/A[i][i];
+		for(int j = 4-1;i < j;--j)
+		{
+			X[i] -= (X[j] * A[i][j]) / A[i][i];
+		}
+	}
+
+	cout<<endl;
+	for (int y = 0; y < n; y++) {
+		cout << X[y] << " ";
+	}
+	cout<<endl;
+	//X[n] = {0};
+
+	/*for(int i = 0; i < n; i++)
 	{
 		for(int j = i+1; j < n; j++)
 		{
@@ -25,7 +67,8 @@ void Gaus(double A[][M],int n)
 			}
 		}
 	}
-	print(A,n,n+1);
+
+	//print_matrix(A, n, n+1);
 
 	for(int i = 0; i < n;i++)
 	{
@@ -47,25 +90,23 @@ void Gaus(double A[][M],int n)
 			X[i] -= (X[j] * A[i][j]) / A[i][i];
 		}
 	}
-	print(A,n,n+1);
+	//print_matrix(A, n, n+1);
 	cout<<endl;
-	for (int y = 0; y < n; y++) {
-		cout << X[y] << " ";
-	}
-}
-void print_matrix(double A[][M],int n, int m)
-{
-
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < 4; i++)
 	{
-		for(int j = 0; j < m; j++)
+		for(int j = 0; j < 5; j++)
 		{
 			cout<<A[i][j]<<" ";
 
 		}
 		cout << endl;
-
 	}
+	cout<<endl;
+	for (int y = 0; y < n; y++) {
+		cout << X[y] << " ";
+	}
+	*/
+	
 }
 void sum(const int A[][M], const int B[][M], int C[][M],int n, int m)
 {
@@ -122,6 +163,59 @@ void get_our_bills(double Input[][M],double ResBills[][M], int n, int m)
 	cout<<endl;
 	print_matrix(ResBills, n, m);
 }
+void find_diff_in_quantity(double Input[][M], double Res[][4])
+{
+	double values[4] = {0.129, 1.461, 85.07, 0.132};
+	//print_matrix(Input, 5, 4);
+	double Input2[4][5];
+	for (int x = 0 ; x < 4; x++)
+	{	
+  		for (int y = 0 ; y < 5; y++)
+  		{
+  			if(y == 4)
+  			{
+  				Input2[x][4] = Input[4][x];
+  			}
+  			else
+  			{
+    			Input2[x][y] = Input[x][y];
+  			}
+  		}
+	}
+	double X[4]; 
+	Gaus(Input2, X, 4);
+
+	for (int i = 1; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			Res[i+1][j] = Res[i+1][j]*(1 - values[i-1]/X[i-1]);
+		}
+	}
+
+	print_matrix(Res, 5, 4);
+
+
+	/*
+	cout<<endl;
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 5; j++)
+		{
+			cout<<Input2[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	*/
+
+
+
+
+	
+	
+
+
+}
 int main()
 {
 	double Input[5][4];
@@ -138,8 +232,9 @@ int main()
 
 	get_our_bills(Input, ResBills, n1, n2);
 
+	double Res[4][5];
 
-
+	find_diff_in_quantity(Input, ResBills);
 
 
 }
