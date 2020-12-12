1340f91277e660b8afa662eb21a0f3761701d6ae
Vladikolev0321 <48590126+Vladikolev0321@users.noreply.github.com>
2020-12-12 17:57:53 +0200

    Update main.cpp

--- MathFundamnetalsOfProgramming/Proekt1/main.cpp
+++ MathFundamnetalsOfProgramming/Proekt1/main.cpp
@@ -6,8 +6,54 @@
 using namespace std;
 
 const int M = 4;
+void Gaus(double A[][M],int n)
+{
+	double X[n] = {0};
 
-void print(double A[][M],int n, int m)
+	for(int i = 0; i < n;i++)
+	{
+		for(int j = i+1; j < n; j++)
+		{
+			if(A[j][i] > A[i][i])
+			{
+				for(int k = 0; k < n+1; k++)
+				{
+					double temp = A[i][k];
+					A[i][k] = A[j][k];
+					A[j][k] = temp;
+				}
+			}
+		}
+	}
+	print(A,n,n+1);
+
+	for(int i = 0; i < n;i++)
+	{
+		for(int j = i+1; j < n; j++)
+		{
+			double del = -(A[j][i]/A[i][i]);
+			for(int k = i; k <= n; k++)
+			{
+				A[j][k] += del * A[i][k];
+			}
+		}
+	}
+
+	for(int i = n-1; i >= 0;i--)
+	{
+		X[i] = A[i][n] / A[i][i];
+		for (int j = n-1; i<j; --j)
+		{
+			X[i] -= (X[j] * A[i][j]) / A[i][i];
+		}
+	}
+	print(A,n,n+1);
+	cout<<endl;
+	for (int y = 0; y < n; y++) {
+		cout << X[y] << " ";
+	}
+}
+void print_matrix(double A[][M],int n, int m)
 {
 
 	for(int i = 0; i < n; i++)
@@ -21,7 +67,17 @@ void print(double A[][M],int n, int m)
 
 	}
 }
-void read_matrix(double A[][M],int n, int m)
+void sum(const int A[][M], const int B[][M], int C[][M],int n, int m)
+{
+	for(int i = 0;i<n;i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+}
+void read_matrix_from_file(double A[][M],int n, int m)
 {
 	string file_name;
 	cin>>file_name;
@@ -49,52 +105,41 @@ void read_matrix(double A[][M],int n, int m)
 	istream.close();
 	
 }
+void get_our_bills(double Input[][M],double ResBills[][M], int n, int m)
+{
+	double values[4] = {0.129, 1.461, 85.07, 0.132};
+
+	for (int i = 0; i < m; i++)
+	{	
+		double sum = 0;
+		for (int j = 0; j < n - 1; j++)
+		{	
+		    sum += Input[i][j] * values[j]; 
+		}
+		ResBills[0][i] = Input[4][i] - sum;
+	}
+
+	cout<<endl;
+	print_matrix(ResBills, n, m);
+}
 int main()
 {
-	/*string file_name;
-	cin>>file_name;
-
-	ifstream istream;
-	istream.open(file_name);
-	*/
-
 	double Input[5][4];
 	
 	int n1,n2;
 	n1 = 5;
 	n2 = 4;
 
-	///read from file
-	/*string file_name;
-	cin>>file_name;
+	read_matrix_from_file(Input, n1, n2);
 
-	ifstream istream;
-	istream.open(file_name);
+	print_matrix(Input, n1, n2);
 
-	for(int i = 0; i < n1; i++)
-	{
-		string line;
-		getline(istream, line);
-		
-		stringstream S(line);
+	double ResBills[5][4];
 
-		for(int j = 0; j < n2; j++)
-		{
-			string curr_num_as_string;
-			getline(S, curr_num_as_string, ' ');
-			//double curr_num = stod(curr_num_as_string);
-			double curr_num = atof(curr_num_as_string.c_str());
-
-			Input[i][j] = curr_num;
-		}	
-	}
-	
-	istream.close();
-//*/
+	get_our_bills(Input, ResBills, n1, n2);
 
 
-	read_matrix(Input, 5, 4);
-	
 
-	print(Input, n1, n2);
+
+
 }
