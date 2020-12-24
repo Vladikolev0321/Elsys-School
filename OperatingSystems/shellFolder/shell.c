f4caf99e86014154ce44c91819c9722c6443cd88
Vladikolev0321 <vladi.kolev2003@gmail.com>
2020-12-24 12:13:09 +0200

    Added executing process. Problems printing perror on new line. Should get the input by getline to try if strtok will work.

--- OperatingSystems/shellFolder/shell.c
+++ OperatingSystems/shellFolder/shell.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 char **parse_cmdline( const char *cmdline)
@@ -37,29 +39,61 @@ char **parse_cmdline( const char *cmdline)
 		count++;
 		//printf("%s\n", curr_tok);
 		//printf("%ld\n", sizeof(curr_tok));
-
 		
 	}
-	////Problem with longer commands realloc();
 
-	//printf("%d\n", count);
-	/*for (int i = 0; i < count; i++)
+	cmd_args = (char**)realloc(cmd_args, (count+1) * sizeof(char*));
+	cmd_args[count] = NULL;
+	////Problem with longer commands realloc();
+	////Should do the parsing part with getline maybe
+	//// not strtok
+
+
+	/*printf("%d\n", count);
+	for (int i = 0; i < count; i++)
 	{
 		printf("%s\n", cmd_args[i]);
 	}
 	*/
 	
+	
 	//printf("Second:%s\n", cmd_args[1]);
 	
 	return cmd_args;
 }
+void run_command(const char *file_name, char **cmd_args)
+{
+	pid_t pid = fork();
 
+	if(pid < 0)
+	{
+		perror("fork");
+	}
+	else if(pid == 0)
+	{
+		if(execv(file_name, cmd_args) == -1)
+		{
+			perror(file_name);
+		}
+		exit(pid);
+	}
+	else if(pid > 0)
+	{
+		int status;
+		if(waitpid(pid, &status, 0) == -1)
+		{
+			perror("wait");
+		}
+	}
+	////////Should i add NULL at the end of 2nd arg
+	/// of execv
+}
 
 int main()
 {
 
 	char *buff = malloc(500 * sizeof(char));
-
+	/////Try reading with getline
 	while(1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
@@ -77,12 +111,14 @@ int main()
 
 		char **cmd_args = parse_cmdline(buff);
 
+		run_command(cmd_args[0], cmd_args);
 		/*for (int i = 0; i < 3; i++)
 		{
 			printf("%s\n", cmd_args[i]);
 		}
 		*/
-
+		free(cmd_args);
+		//////free buffer
 	}
 
 
