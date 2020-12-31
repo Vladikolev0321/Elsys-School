347358ade1f9cbe65dba7866da5ab37f04e71d6a
Vladikolev0321 <vladi.kolev2003@gmail.com>
2020-12-31 19:23:01 +0200

    Made writing and reading from file. For Proj2 of MOPR.

---
+++ MathFundamnetalsOfProgramming/Project2/proj2.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+class Point
+{
+public:
+	int x;
+	int y;
+	Point(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+class Table
+{
+	list<Point> points;
+	int count_points;
+public:
+	Table()
+	{
+		count_points = 0;
+	};
+	void add_point(Point p)
+	{
+		this->points.push_back(p);
+		count_points++;
+	}
+	void print_points()
+	{
+		for (list<Point>::iterator it = this->points.begin(); it != this->points.end(); it++)
+		{
+			Point curr_point = *it;
+			cout << curr_point.x << ',' << curr_point.y <<endl;
+		}
+	}
+
+};
+void read_from_file(Table &table)
+{
+	string file_name = "proj2.txt";
+
+	ifstream istream;
+	istream.open(file_name);
+
+	
+	string line1;
+	getline(istream, line1);
+
+	stringstream S(line1);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		string curr_num_as_string;
+		getline(S, curr_num_as_string, ',');
+		int curr_num = stoi(curr_num_as_string.c_str());
+
+		string curr_num_as_string2;
+		getline(S, curr_num_as_string2, ' ');
+		int curr_num2 = stoi(curr_num_as_string2.c_str());
+
+		//cout << curr_num << ',' << curr_num2 <<endl;
+
+		Point curr_point(curr_num, curr_num2);
+		table.add_point(curr_point);
+
+	}
+	
+	
+
+	//for()
+}
+void write_to_file()
+{
+	string file_name = "proj2.txt";
+
+	ofstream ostream;
+	ostream.open(file_name);
+
+	string table_coorrdinates;
+	getline(cin, table_coorrdinates);
+	//cin>>table_coorrdinates;
+
+	string diameter_of_ball;
+	getline(cin, diameter_of_ball);
+	//cin>>diameter_of_ball;
+
+	string ball_coordinates;
+	getline(cin, ball_coordinates);
+	//cin>>ball_coordinates;
+
+
+	ostream << table_coorrdinates << endl;
+	ostream << diameter_of_ball << endl;
+	ostream<<ball_coordinates << endl;
+
+	ostream.close();
+}
+int main()
+{
+	/*
+	0,0 320,0 320,160 0,160
+	0
+	280,70
+	*/
+	cout<<"Do you want to change the coordinates:(yes/no)"<<endl;
+	string answer;
+	getline(cin,answer);
+	if(answer == "yes")
+	{
+		write_to_file();	
+	}
+
+	Table table;
+	read_from_file(table);
+
+	table.print_points();
+
+
+	return 0;
+}
