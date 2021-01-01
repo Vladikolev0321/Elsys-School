859854b8700c6751adc1decc01ea83d4c908f0e0
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-01-01 22:50:49 +0200

    Updated proj2

--- MathFundamnetalsOfProgramming/Project2/proj2.cpp
+++ MathFundamnetalsOfProgramming/Project2/proj2.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <fstream>
 #include <sstream>
+#include <math.h>
+#include <vector>
 using namespace std;
 
 class Point
@@ -19,7 +21,7 @@ public:
 ///To add Ball class
 class Table
 {
-	list<Point> points;
+	vector<Point> points;
 	int count_points;
 public:
 	Table()
@@ -39,61 +41,110 @@ public:
 			cout << curr_point.x << ',' << curr_point.y <<endl;
 		}
 	}
+	int check_if_rectangle()
+	{
+		int first_length = sqrt( pow(points[2].x - points[1].x, 2) + pow(points[2].y - points[1].y, 2);
+		int second_length = sqrt( pow(points[4].x - points[3].x, 2) + pow(points[4].y - points[3].y, 2);
+
+		int first_width = sqrt( pow(points[2].x - points[1].x, 2) + pow(points[2].y - points[1].y, 2);
+		int second_width = sqrt( pow(points[4].x - points[3].x, 2) + pow(points[4].y - points[3].y, 2);
+		
+		if(first_length == second_length && first_width == second_width
+		 && first_width * 2 == first_length)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+		
+	}
+};
+class Ball
+{
+	int x;
+	int y;
+	int diameter;
+public:
+	Ball(){};
+	Ball(int x, int y, int diameter)
+	{
+		this->x = x;
+		this->y = y;
+		this->diameter = diameter;
+	}
+	int get_x()
+	{
+		return x;
+	}
+	int get_y()
+	{
+		return y;
+	}
+	int get_diameter()
+	{
+		return diameter;
+	}
+
 };
 
-void read_from_file(Table &table)
+void read_from_file(Table &table, Ball &ball)
 {
 	string file_name = "proj2.txt";
 
 	ifstream istream;
 	istream.open(file_name);
 
-	
+///coordinates of table
+
 	string line1;
 	getline(istream, line1);
 
 	stringstream S(line1);
 
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; i++)
 	{
 		string curr_num_as_string;
 		getline(S, curr_num_as_string, ',');
-		int curr_num = stoi(curr_num_as_string.c_str());
+		int curr_num = stoi(curr_num_as_string);
 
 		string curr_num_as_string2;
 		getline(S, curr_num_as_string2, ' ');
-		int curr_num2 = stoi(curr_num_as_string2.c_str());
+		int curr_num2 = stoi(curr_num_as_string2);
 
 		//cout << curr_num << ',' << curr_num2 <<endl;
 
 		Point curr_point(curr_num, curr_num2);
 		table.add_point(curr_point);
-
 	}
+//// to check if table is rectangle
+/// func lenght of vector from 2 points
+/// 
 ///diameter
 	string line2;
 	getline(istream, line2);
 
-	stringstream S(line2);
+	stringstream S1(line2);
 	string diameter_of_ball_as_string;
-	getline(S, diameter_of_ball_as_string, ' ');
-	int diameter = stoi(diameter_of_ball_as_string.c_str());
+	getline(S1, diameter_of_ball_as_string, ' ');
+	int diameter = stoi(diameter_of_ball_as_string);
 
 ////coordinates of ball
 	string line3;
 	getline(istream, line3);
 
-	stringstream S(line3);
+	stringstream S3(line3);
 
 	string x_as_string;
-	getline(S, x_as_string, ',');
-	int x_coordinate = stoi(x_as_string.c_str());
+	getline(S3, x_as_string, ',');
+	int x_coordinate = stoi(x_as_string);
 
 	string y_as_string;
-	getline(S, y_as_string, ' ');
-	int y_coordinate = stoi(y_as_string.c_str());
-	
+	getline(S3, y_as_string, ' ');
+	int y_coordinate = stoi(y_as_string);
 
+	ball = Ball(x_coordinate, y_coordinate, diameter);
 	//for()
 }
 void write_to_file()
@@ -138,9 +189,17 @@ int main()
 	}
 
 	Table table;
-	read_from_file(table);
+	Ball ball;
+	read_from_file(table, ball);
 
-	table.print_points();
+
+	//Printing Ball
+	cout<<ball.get_x()<<endl;
+	cout<<ball.get_y()<<endl;
+	cout<<ball.get_diameter()<<endl;
+
+	//Printing table
+	//table.print_points();
 
 
 	return 0;
