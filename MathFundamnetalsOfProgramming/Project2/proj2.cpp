f8f9b2b5f29f908cd8a30be9a0f3549a01e0f476
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-01-09 22:24:07 +0200

    Updated proj2

--- MathFundamnetalsOfProgramming/Project2/proj2.cpp
+++ MathFundamnetalsOfProgramming/Project2/proj2.cpp
@@ -52,13 +52,13 @@ public:
 	}
 	int check_if_rectangle()
 	{
-		int first_length = sqrt( pow(points[1].x - points[0].x, 2) + pow(points[1].y - points[0].y, 2) );
-		int second_length = sqrt( pow(points[2].x - points[3].x, 2) + pow(points[2].y - points[3].y, 2) );
+		double first_length = sqrt( pow(points[1].x - points[0].x, 2) + pow(points[1].y - points[0].y, 2) );
+		double second_length = sqrt( pow(points[2].x - points[3].x, 2) + pow(points[2].y - points[3].y, 2) );
 
-		int first_width = sqrt( pow(points[2].x - points[1].x, 2) + pow(points[2].y - points[1].y, 2) );
-		int second_width = sqrt( pow(points[3].x - points[0].x, 2) + pow(points[3].y - points[0].y, 2) );
+		double first_width = sqrt( pow(points[2].x - points[1].x, 2) + pow(points[2].y - points[1].y, 2) );
+		double second_width = sqrt( pow(points[3].x - points[0].x, 2) + pow(points[3].y - points[0].y, 2) );
 		
-
+		
 		///1 lenght and 1 width /// 
 		/// ab||ox  ad || oy 2 points width0.y == width1.y width0.x != width1.x
 		// 2 points lenght0.y != lenght3.y lenght0.x == length3.x
@@ -68,10 +68,16 @@ public:
 		// a = (b1.y-a0.y)/(b1.x - a0.x) (pri ab)
 		// calculate a for AD
 		// aAB * aAD == -1 => AB perpendik AD
+		/*cout<<first_length<<endl;
+		cout<<second_length<<endl;
+		cout<<first_width<<endl;
+		cout<<second_width<<endl;
+		*/
 
 		if(first_length == second_length && first_width == second_width
 		 && first_width * 2 == first_length)
-		{
+		{///y1 == y2
+			cout<<1<<endl;
 			if(points[0].y == points[1].y && points[0].x != points[1].x
 				&& points[0].y != points[3].y && points[0].x == points[3].x)
 			{
@@ -79,8 +85,11 @@ public:
 			}
 			else
 			{
+				cout<<2<<endl;
 				int koef1 = (points[1].y - points[0].y)/(points[1].x - points[0].x);
 				int koef2 = (points[3].y - points[0].y)/(points[3].x - points[0].x);
+				cout<<koef1<<endl;
+				cout<<koef1<<endl;
 
 				if(koef1 * koef2 == -1)
 				{
@@ -102,6 +111,11 @@ public:
 		///after that to move the ball
 	}
 };
+double get_vector_length(Point p1, Point p2)
+{
+	double length = sqrt( pow(p2.x - p1.x, 2) + pow(p1.y, 2) );
+	return length;
+}
 class Ball
 {
 	double x;
@@ -109,6 +123,8 @@ class Ball
 	double diameter;
 	double start_x;
 	double start_y;
+	double ukoef;
+	double skoef;
 public:
 	Point direction;
 	float strength;
@@ -121,15 +137,15 @@ public:
 		this->y = y;
 		this->diameter = diameter;
 	}
-	int get_x()
+	double get_x()
 	{
 		return x;
 	}
-	int get_y()
+	double get_y()
 	{
 		return y;
 	}
-	int get_diameter()
+	double get_diameter()
 	{
 		return diameter;
 	}
@@ -151,12 +167,13 @@ public:
 		double length_of_final_vector = (this->strength * min_length) / 2;
 
 		return length_of_final_vector;
-	}
+	}*/
 	double find_length_of_direction_vector()
 	{
 		double length_of_direction_vector = sqrt( pow(direction.x - this->x, 2) + pow(direction.y - this->y, 2) );
 		return length_of_direction_vector; 
 	}
+	/*
 	void move(Table table)
 	{
 		double koef = find_length_of_direction_vector() / find_length_of_vector_which_the_ball_will_move(table);
@@ -169,21 +186,129 @@ public:
 		// set x and y of ball to final point coordiantes;
 	}
 	*/
+
 	void move(Table table)
 	{
 		////////Change koef
-		double koef = 1 / this->strength;
+		//double koef = 1 / this->strength;
 
 		//// x1 = (1 - koef) * this->x + koef
 		//// (xt(direction.x) - (1 - koef)*this->x)/ t = x1
 		//// (yt(direction.y) - (1 - koef)*this->y)/ t = y2
+		
+		/*double vector_length = find_length_of_direction_vector();
+		double W = sqrt( pow(table.points[1].x - table.points[0].x, 2) + pow(table.points[1].y - table.points[0].y, 2) );
+		//koef
+		if(vector_length >= W / 10 && vector_length <= W*3/10)
+		{
+			cout<<"The length should be in range W/10 and W*3/10"<<endl;
+			return;
+		}
+		*/
 
-		Point final_point(round((direction.x - (1 - koef)*this->x)/ koef) , round((direction.y - (1 - koef)*this->y)/ koef));
+		//Point final_point(round((direction.x - (1 - koef)*this->x)/ koef) , round((direction.y - (1 - koef)*this->y)/ koef));
+		Point final_point((direction.x - this->x)*this->strength+this->x, (direction.y - this->y)*this->strength+this->y);
 		////TO DO
 		cout<<final_point.x<<" "<<final_point.y<<endl; 
 
+
+		
+
+		this->ukoef = (this->direction.y - this->y)/(this->direction.x - this->x);
+
+		this->skoef = this->y - this->ukoef*this->x;
+
+		double table_down_y = table.points[0].y;
+		double table_up_y = table.points[2].y;
+		double table_left_x = table.points[0].x;
+		double table_right_x = table.points[1].x;
+		//double table_x = table.points[0].x;
+
+		int rad = this->diameter/2;
+		//cout<<table_down_y<<endl;
+
+		//check if ball is in the corner
+		double final_length = get_vector_length(Point(this->x, this->y), Point(final_point.x, final_point.y));
+		// y = ax + b
+		// 
+		for (int i = 0; i < table.points.size(); i++)
+		{
+			double y = this->ukoef * table.points[i].x + this->skoef;
+			if(y == table.points[i].y)
+			{
+				double curr_corner_length = get_vector_length(Point(this->x, this->y), table.points[i]);
+				if(final_length >= curr_corner_length )
+				{
+					cout<<"The ball hit the corner"<<endl;
+					this->x = this->start_x;
+					this->y = this->start_y;
+					return;
+				}
+				
+			}
+		}
+		
+		
+		//down
+		if(final_point.y < table_down_y)
+		{
+		  /*double xCollision=(bottom-newHit.trajectoryB+newHit.hitBall.radius)/newHit.trajectoryA;
+            cout<<"The ball bounced into the wall ("<<xCollision<<","<<bottom+newHit.hitBall.radius<<")"<<endl;
+            newHit.newY=-newHit.newY+2*newHit.hitBall.radius;
+			*/
+			double sbl_x=(table_down_y-this->skoef+rad)/this->ukoef;
+            cout<<"The ball bounced into the wall ("<<sbl_x<<","<<table_down_y+rad<<")"<<endl;
+            final_point.y= -final_point.y+this->diameter;
+
+			this->x = final_point.x;
+			this->y = final_point.y;
+
+			cout<<final_point.x<<" "<<final_point.y<<endl; 
+
+		}//up
+		else if(final_point.y > table_up_y)
+		{
+			double sbl_x=(table_up_y-this->skoef+rad)/this->ukoef;
+            cout<<"The ball bounced into the wall ("<<sbl_x<<","<<table_up_y+rad<<")"<<endl;
+            final_point.y= -final_point.y+this->diameter;
+
+			this->x = final_point.x;
+			this->y = final_point.y;
+			
+			cout<<final_point.x<<" "<<final_point.y<<endl;
+		}//left
+		else if(final_point.x < table_left_x)
+		{
+			double sbl_y=(table_left_x-this->skoef+rad)/this->ukoef;
+            cout<<"The ball bounced into the wall ("<<sbl_y<<","<<table_left_x+rad<<")"<<endl;
+            final_point.x= -final_point.x+this->diameter;
+			
+			this->x = final_point.x;
+			this->y = final_point.y;
+
+			cout<<final_point.x<<" "<<final_point.y<<endl;
+		}
+		else if(final_point.x > table_right_x)
+		{
+			double sbl_y=(table_right_x-this->skoef+rad)/this->ukoef;
+            cout<<"The ball bounced into the wall ("<<sbl_y<<","<<table_right_x+rad<<")"<<endl;
+            final_point.x= -final_point.x+this->diameter;
+			
+			this->x = final_point.x;
+			this->y = final_point.y;
+
+			cout<<final_point.x<<" "<<final_point.y<<endl;
+		}
+		else
+		{
+			this->x = final_point.x;
+			this->y = final_point.y;
+		}
+
+		/// y = a.x + b
+		/// 
 		//// Check if ball hits the corner
-		for(int i = 0; i < table.points.size(); i++)
+		/*for(int i = 0; i < table.points.size(); i++)
 		{
 			if(table.points[i].x == final_point.x && table.points[i].y == final_point.y)
 			{	
@@ -192,12 +317,14 @@ public:
 				return;
 			}
 		}
+		*/
 		
 
 	}
 
 
 };
+
 
 void read_from_file(Table &table, Ball &ball)
 {
@@ -304,6 +431,7 @@ int main()
 	Table table;
 	Ball ball;
 	read_from_file(table, ball);
+	table.print_points();
 	while (table.check_if_rectangle() != 1)
 	{
 		write_to_file();
@@ -312,12 +440,13 @@ int main()
 	
 
 	//Printing Ball
-	cout<<ball.get_x()<<endl;
+	/*cout<<ball.get_x()<<endl;
 	cout<<ball.get_y()<<endl;
 	cout<<ball.get_diameter()<<endl;
-
+	*/
 	//Printing table
 	table.print_points();
+	
 
 	//input sila, direction.x direction.y    primer 1: 2     230      110   
 	cout<<"Sila:"<<endl;
@@ -326,6 +455,11 @@ int main()
 	cin>>strenght;
 	cin>>direction_x;
 	cin>>direction_y;
+	while(strenght<2||strenght>5)
+	{
+		cout<<"Strenght should be between 2 and 5"<<endl;
+		cin>>strenght;
+	}
 	cout<<strenght<<"|"<<direction_x<<"|"<<direction_y<<endl;
 
 	ball.direction.x = direction_x;
