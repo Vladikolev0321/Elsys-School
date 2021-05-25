bd2c3c86f4dbfa4e793fb029572563326da04801
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-05-25 12:32:55 +0300

    near the end changes

--- MathFundamnetalsOfProgramming/Graphs/Проект 3/main.cpp
+++ MathFundamnetalsOfProgramming/Graphs/Проект 3/main.cpp
@@ -24,10 +24,10 @@ void print(vector<vector<int>> v) {
 }
 
 void find_all_paths_from_starting_point(int start_pos, int curr_pos,  vector<int> curr_path){
+    //pushes the current node in the path
     curr_path.push_back(curr_pos);
-    
-    vector<bool> is_visited;
 
+    vector<bool> is_visited;
     for (int i = 0; i < G.size(); i++){
         is_visited.push_back(false);
     }
@@ -43,77 +43,102 @@ void find_all_paths_from_starting_point(int start_pos, int curr_pos,  vector<int
         }
     }
 
-    if(are_all_visited){
-        if(G[curr_pos][start_pos] > 0){ // if it is -1 or 0 to not count as path
-            all_paths.push_back(curr_path);
-        }
-    }else{
+    /* if all nodes are not visited we are going through other connected nodes to the current node and checking if they are visited
+       if they are not we invoke the function recurcively from the next connected unvisited node 
+    */
+    /*
+       if all nodes in the graph are visited we check if there is path back to the start and if there is we add it as a valid path
+    */
+    if(!are_all_visited){
         for (int i = 0; i < connected_nodes[curr_pos].size(); i++){
             if(!is_visited[connected_nodes[curr_pos][i]]){
                 find_all_paths_from_starting_point(start_pos, connected_nodes[curr_pos][i], curr_path);
+            }
+        }
+    }else{
+        
+        if(G[curr_pos][start_pos] > 0){ // if it is -1 or 0 to not count as path because it has no path back to the start node
+
+            // checking if some of the graphs path has -1 as path and if so doesn't add it to the valid paths
+            for (int j = 0; j < curr_path.size()-1; j++){ 
+
+                if(G[curr_path[j]][curr_path[j+1]] == -1){
+                    for (int i = 0; i < curr_path.size(); i++){
+                        curr_path[i] = 0;
+                    }
+                return;
+                }
+            }
+            
+            all_paths.push_back(curr_path);
+            for (int i = 0; i < curr_path.size(); i++){
+                curr_path[i] = 0;
             }
         }
         
     }
 }
 
-void find_shortest_path_from_start_to_end(int start_pos, int end_pos, int curr_pos,  vector<int> curr_path){
-    // if(curr_pos == end_pos){
-    //     return;
-    // }
-    curr_path.push_back(curr_pos);
+// void find_shortest_path_from_start_to_end(int start_pos, int end_pos, int curr_pos,  vector<int> curr_path){
+//     // if(curr_pos == end_pos){
+//     //     return;
+//     // }
+//     curr_path.push_back(curr_pos);
     
-    vector<bool> is_visited;
+//     vector<bool> is_visited;
 
-    for (int i = 0; i < G.size(); i++){
-        is_visited.push_back(false);
-    }
-    for (int j = 0; j < curr_path.size(); j++){
-        is_visited[curr_path[j]] = true;
-    }
+//     for (int i = 0; i < G.size(); i++){
+//         is_visited.push_back(false);
+//     }
+//     for (int j = 0; j < curr_path.size(); j++){
+//         is_visited[curr_path[j]] = true;
+//     }
 
-    /// check if all nodes in the graph are visited
-    bool are_all_visited = true;
-    for (int i = 0; i < G.size(); i++){
-        if(!is_visited[i]){
-            are_all_visited = false;
-        }
-    }
+//     /// check if all nodes in the graph are visited
+//     bool are_all_visited = true;
+//     for (int i = 0; i < G.size(); i++){
+//         if(!is_visited[i]){
+//             are_all_visited = false;
+//         }
+//     }
 
-    if(are_all_visited){
-        if(G[curr_pos][start_pos] > 0){ // if it is -1 or 0 to not count as path
-            from_start_to_end.push_back(curr_path);
-        }
-    }else{
-        for (int i = 0; i < connected_nodes[curr_pos].size(); i++){
-            cout<<"in"<<endl;
-            if(!is_visited[connected_nodes[curr_pos][i]] && connected_nodes[curr_pos][i] != end_pos){
-                find_shortest_path_from_start_to_end(start_pos, end_pos, connected_nodes[curr_pos][i], curr_path);
-             }else if(connected_nodes[curr_pos][i] == end_pos){
-                 curr_path.push_back(connected_nodes[curr_pos][i]);
-                 return;
-             }
-        }
+//     if(are_all_visited){
+//         if(G[curr_pos][start_pos] > 0){ // if it is -1 or 0 to not count as path
+//             from_start_to_end.push_back(curr_path);
+            
+            
+//         }
+//     }else{
+//         for (int i = 0; i < connected_nodes[curr_pos].size(); i++){
+//             cout<<"in"<<endl;
+//             if(!is_visited[connected_nodes[curr_pos][i]] && connected_nodes[curr_pos][i] != end_pos){
+//                 find_shortest_path_from_start_to_end(start_pos, end_pos, connected_nodes[curr_pos][i], curr_path);
+//              }else if(connected_nodes[curr_pos][i] == end_pos){
+//                  curr_path.push_back(connected_nodes[curr_pos][i]);
+//                  return;
+//              }
+//         }
         
-    }
-}
+//     }
+// }
 
-void calculate_shortest_path(){
+void calculate_shortest_path(vector<vector<int>> paths){
     vector<int> all_paths_sum;
-    for (int i = 0; i < all_paths.size(); i++){
+    // for (int i = 0; i < paths.size(); i++){
+    //     all_paths_sum.push_back(0);
+    // }
+
+    for (int i = 0; i < paths.size(); i++){
+        vector<int> curr_path = paths[i];
         all_paths_sum.push_back(0);
-    }
-
-    for (int i = 0; i < all_paths.size(); i++){
-
-        vector<int> curr_path = all_paths[i];
-        for (int j = 0; j < curr_path.size()-1; j++){
+        for (int j = 0; j < curr_path.size()-1; j++){ // adding the sum of the path between curr node and next node in path
             all_paths_sum[i] += G[curr_path[j]][curr_path[j+1]];
         }
+        // finally adding the last 
         all_paths_sum[i] += G[curr_path[curr_path.size()-1]][curr_path[0]];
     }
 
-    // finding the minimal path
+    // finding the minimal path and min index
     int min = all_paths_sum[0];
     int min_index = 0;
     for (int i = 0; i < all_paths_sum.size(); i++){
@@ -123,7 +148,7 @@ void calculate_shortest_path(){
         }
     }
     
-    vector<int> shortest_path = all_paths[min_index];
+    vector<int> shortest_path = paths[min_index];
     
     
     cout<<"Path: ";
@@ -133,10 +158,23 @@ void calculate_shortest_path(){
     cout<<"Time: "<<min<<endl;
 
 }
+bool is_there_full_path_with_this_starting_point(int start_pos){
+    all_paths.clear();
+    vector<int> curr_path;
+    find_all_paths_from_starting_point(start_pos, start_pos, curr_path);
+    if(all_paths.size() == 0){
+        return false;
+    }
+    return true;
 
+}
   //fix argc and argv
 int main(int argc, char *argv[])
 {
+    if(argc != 2){
+        cout<<"You didn't provide a file";
+        return 0;
+    }
     string file_name = argv[1];
 
   //  int count_vertexes;
@@ -188,14 +226,18 @@ int main(int argc, char *argv[])
     find_all_paths_from_starting_point(start_node, start_node, curr_path);
     print(all_paths);
 
-    calculate_shortest_path();
+    calculate_shortest_path(all_paths);
     
+    for (int i = 0; i < curr_path.size(); i++)
+    {
+        cout<<curr_path[i]<<endl;
+    }
     
     vector<int> curr_path2;
     cout<<"here"<<endl;
-    find_shortest_path_from_start_to_end(start_node, 3, start_node, curr_path2);
-    print(from_start_to_end);
-
+    //find_shortest_path_from_start_to_end(start_node, 3, start_node, curr_path2);
+    //print(from_start_to_end);
+    cout<<"Is there full path with this starting node:"<<is_there_full_path_with_this_starting_point(start_node)<<endl;
     
     
 }
